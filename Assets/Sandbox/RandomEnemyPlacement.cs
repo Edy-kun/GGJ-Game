ce32765a375bafc487f0cf6539c9cd6e0ad5eeaa
Edy van Tol <info@edyvantol.nl>
2020-01-31 23:50:41 +0100

    TerrainAsset
    Removed TerrainAutoUpgrade, Debug voor terrain Asset

---
+++ Assets/Sandbox/RandomEnemyPlacement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomEnemyPlacement : MonoBehaviour
+{
+    [SerializeField] private GameObject crocPrefab;
+    [SerializeField] private float minSpawnRange, maxSpawnRange;
+
+    private void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, minSpawnRange);
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, minSpawnRange);
+    }
+}
