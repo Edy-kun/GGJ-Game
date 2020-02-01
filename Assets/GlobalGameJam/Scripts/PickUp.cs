e1196db6a31c518ee05cf7372f91c46ad08f5bf7
Therrato <r.schroder@live.nl>
2020-02-01 10:49:54 -0800

    PickUpProfile

--- Assets/GlobalGameJam/Scripts/PickUp.cs
+++ Assets/GlobalGameJam/Scripts/PickUp.cs
@@ -2,13 +2,20 @@
 
 public class PickUp : MonoBehaviour
 {
-    private Element _cointains;
+    private PickUpProfile _contains;
+
+    public void SetProfile(PickUpProfile contain)
+    {
+        _contains = contain;
+        GameObject.Instantiate(_contains.GameObject);
+    }
+    
     private void OnCollisionEnter(Collision other)
     {
         var collidedWith = other.transform.GetComponent<ICanPickUp>();
         if (collidedWith == null) 
             return;
-        if (collidedWith.TryPickUp(_cointains))
+        if (collidedWith.TryPickUp(_contains.Element))
         {
             Destroy(gameObject);
         }
