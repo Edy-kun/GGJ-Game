f82178bcaea38583cfca04b427275b4f2e59a597
Edy van Tol <info@edyvantol.nl>
2020-02-02 12:29:01 +0100

    BOmber
    Bomber

---
+++ Assets/GlobalGameJam/Entities/EnemyHealth.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int totalHealth;
+    public int currentHealth;
+
+    [SerializeField] private GameObject explosion;
+
+    private void Start()
+    {
+        currentHealth = totalHealth;
+    }
+
+    private void Update()
+    {
+        if (currentHealth == 0 || currentHealth < 0)
+        {
+            Instantiate(explosion, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+
+    public void DoDamage(int damage)
+    {
+        currentHealth -= damage;
+    }
+}
