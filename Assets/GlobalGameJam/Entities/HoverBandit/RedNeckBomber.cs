9fea55b3d4a31759f45e4e4b30442d8c58a6c01f
Edy van Tol <info@edyvantol.nl>
2020-02-02 08:33:58 +0100

    f
    Ga weg wouter

---
+++ Assets/GlobalGameJam/Entities/HoverBandit/RedNeckBomber.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.AI;
+
+public class RedNeckBomber : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent agent;
+    [SerializeField] private Transform[] points;
+
+    [SerializeField] private GameObject minePrefab;
+
+    private int destPoint = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        GotoNextPoint();
+
+        InvokeRepeating("ThrowRandomMine", 5f, 5f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateAgent();
+    }
+
+    private void ThrowRandomMine()
+    {
+        Instantiate(minePrefab,transform.localPosition +  new Vector3(0,1f,-5f), Quaternion.identity);
+    }
+
+    private void UpdateAgent()
+    {
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+        {
+            GotoNextPoint();
+        }
+    }
+
+    private void GotoNextPoint()
+    {
+        // Returns if no points have been set up
+        if (points.Length == 0)
+            return;
+
+        // Set the agent to go to the currently selected destination.
+        agent.destination = points[destPoint].position;
+
+        // Choose the next point in the array as the destination,
+        // cycling to the start if necessary.
+        destPoint = (destPoint + 1) % points.Length;
+    }
+
+    private void OnDrawGizmos()
+    {
+        for (int i = 0; i < points.Length; i++)
+        {
+            Gizmos.color = Color.yellow;
+            Handles.Label(points[i].position, points[i].name);
+            Gizmos.DrawSphere(points[i].position, 0.25f);
+        }
+    }
+}
